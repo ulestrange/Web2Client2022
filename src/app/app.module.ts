285b5f6a511a23da6bc3c9b17064bb2d25493924
Una <una.lestrange@atu.ie>
2022-11-27 17:47:09 +0000

    Revert "saved for move to other machine"
    This reverts commit e26a791b95d612eeb1be08be559bb582ab7ce23c.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
-
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
@@ -28,8 +24,7 @@ import {BookFormComponent } from './book/book-form/book-form.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule,
-    AmplifyAuthenticatorModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
